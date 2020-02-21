a52e23771b9fc411d9e7a3bb458f7bcd6d4aa58e
Pavan Gandhi <pavangandhi100@gmail.com>
2020-02-21 12:07:36 +0530

    added lot of things

--- script.js
+++ script.js
@@ -34,3 +34,26 @@ function toggleAccordion(){
 }
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let devfolioOptions = {
+        buttonSelector: '#devf
+olio-apply-now',
+        key: 'hackrcdu',
+    }
+
+    let script = document.createElement('script');
+    script.src = "https://apply.devfolio.co";
+    document.head.append(script);
+
+    script.onload = function () {
+        new Devfolio(devfolioOptions);
+    }
+
+    script.onerror = function () {
+        document.querySelector(devfolioOptions.buttonSelector).addEventListener('click', function () {
+            window.location.href = 'https://devfolio.co/external-apply/' + devfolioOptions.key;
+        });
+    }
+});
